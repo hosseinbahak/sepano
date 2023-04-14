19b861b03d0386665f6ad617c78341892b1e7a6f
hossein bahak <hossein.bahak@gmail.com>
2023-04-14 23:29:19 +0330

    custom login api changed

--- account/views.py
+++ account/views.py
@@ -18,52 +18,52 @@ class UserRegister(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        #try:
-        user_obj, user_bool = User.objects.get_or_create(
-            username = unidecode(serializer.validated_data.get("mobile"))
-        )
-        if user_bool:
-            password = unidecode(serializer.validated_data.get("password"))
-            hashed_password = make_password(password)
-            user_obj.password = hashed_password
-            user_obj.first_name = serializer.validated_data.get("first_name")
-            user_obj.last_name = serializer.validated_data.get("last_name")
-            user_obj.email = serializer.validated_data.get("email")
-            user_obj.save()
+        try:
+            user_obj, user_bool = User.objects.get_or_create(
+                username = unidecode(serializer.validated_data.get("mobile"))
+            )
+            if user_bool:
+                password = unidecode(serializer.validated_data.get("password"))
+                hashed_password = make_password(password)
+                user_obj.password = hashed_password
+                user_obj.first_name = serializer.validated_data.get("first_name")
+                user_obj.last_name = serializer.validated_data.get("last_name")
+                user_obj.email = serializer.validated_data.get("email")
+                user_obj.save()
+                
+
+            user_profile_obj, user_profile_bool = UserProfile.objects.get_or_create(
+                user = user_obj,   
+            )
+            if user_profile_bool:
+                user_profile_obj.gender = serializer.validated_data.get("gender")
+                user_profile_obj.birth_date = serializer.validated_data.get("birth_date")
+                user_profile_obj.save()
             
+            user = authenticate(
+                username = unidecode(serializer.validated_data.get("mobile")),
+                password = unidecode(serializer.validated_data.get("password"))
+            )
 
-        user_profile_obj, user_profile_bool = UserProfile.objects.get_or_create(
-            user = user_obj,   
-        )
-        if user_profile_bool:
-            user_profile_obj.gender = serializer.validated_data.get("gender")
-            user_profile_obj.birth_date = serializer.validated_data.get("birth_date")
-            user_profile_obj.save()
-        
-        user = authenticate(
-            username = unidecode(serializer.validated_data.get("mobile")),
-            password = unidecode(serializer.validated_data.get("password"))
-        )
+            if user is not None:
+                login(request, user)
+                refresh = RefreshToken.for_user(user)
+                token = str(refresh.access_token)
+                return Response(
+                                response_func(True, "user registered successfully", {'access': token, 'refresh': str(refresh)}),  
+                                status=status.HTTP_200_OK
+                            )
+            else:
+                return Response(
+                                response_func(True, "login failed", {'user_exist': False}),  
+                                status=status.HTTP_401_UNAUTHORIZED
+                            )
 
-        if user is not None:
-            login(request, user)
-            refresh = RefreshToken.for_user(user)
-            token = str(refresh.access_token)
+        except Exception as e:
             return Response(
-                            response_func(True, "user registered successfully", {'access': token, 'refresh': str(refresh)}),  
-                            status=status.HTTP_200_OK
-                        )
-        else:
-            return Response(
-                            response_func(True, "login failed", {'user_exist': False}),  
-                            status=status.HTTP_401_UNAUTHORIZED
-                        )
-
-        # except Exception as e:
-        #     return Response(
-        #                     response_func(False, str(e), {'user_exist': False}),  
-        #                     status=status.HTTP_400_BAD_REQUEST
-        #         )
+                            response_func(False, str(e), {'user_exist': False}),  
+                            status=status.HTTP_400_BAD_REQUEST
+                )
 
 class UserLogin(generics.GenericAPIView):
     serializer_class = UserLoginSerializer
